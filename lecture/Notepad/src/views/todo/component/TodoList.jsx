4ad2a926bbc1c6e32a350fbbeca706e8905523db
yjkwon07 <dudwns9073@nate.com>
2020-09-13 11:49:20 +0900

    (PA) lecture [Notepad > todo]: - react-virtualized add - 스크롤되기 전 보이지 않는 컴포넌트는 렌더링하지 않고 크기만 차지

--- lecture/Notepad/src/views/todo/component/TodoList.jsx
+++ lecture/Notepad/src/views/todo/component/TodoList.jsx
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { List } from 'react-virtualized';
 import TodoListItem from './TodoListItem';
 import '../../../assets/scss/todo/TodoList.scss';
 
 function TodoList(props) {
   const { handleRemove, toggleTodoCheck } = props;
   const { todos } = props;
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
+        <TodoListItem
+          todo={todo}
+          key={key}
+          handleRemove={handleRemove}
+          toggleTodoCheck={toggleTodoCheck}
+          style={style}
+        />
+      );
+    },
+    [handleRemove, toggleTodoCheck, todos],
+  );
+
   return (
-    <div className="TodoList">
-      {todos &&
-        todos.map((todo) => (
-          <TodoListItem
-            todo={todo}
-            handleRemove={handleRemove}
-            toggleTodoCheck={toggleTodoCheck}
-            key={todo.id}
-          />
-        ))}
-    </div>
+    <List
+      className="TodoList"
+      list={todos}
+      rowCount={todos.length}
+      width={512}
+      height={513}
+      rowHeight={57}
+      rowRenderer={rowRenderer}
+      style={{ outline: 'none' }}
+    />
   );
 }
 
--- lecture/Notepad/src/views/todo/component/TodoListItem.jsx
+++ lecture/Notepad/src/views/todo/component/TodoListItem.jsx
@@ -9,19 +9,21 @@ import '../../../assets/scss/todo/TodoListItem.scss';
 
 function TodoListItem(props) {
   const { handleRemove, toggleTodoCheck } = props;
-  const { todo } = props;
+  const { todo, style } = props;
   const { id, text, checked } = todo;
   return (
-    <div className="TodoListItem">
-      <div
-        className={cn('checkbox', { checked })}
-        onClick={() => toggleTodoCheck(id)}
-      >
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="text">{text}</div>
-      </div>
-      <div className="remove" onClick={() => handleRemove(id)}>
-        <MdRemoveCircleOutline />
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div
+          className={cn('checkbox', { checked })}
+          onClick={() => toggleTodoCheck(id)}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={() => handleRemove(id)}>
+          <MdRemoveCircleOutline />
+        </div>
       </div>
     </div>
   );
