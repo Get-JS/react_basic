78c2d716295fd9c0d2dc36fee06726787b2b5e26
yjkwon07 <dudwns9073@nate.com>
2020-09-27 11:59:54 +0900

    (PA) lecture [Notepad > todos]: - react-redux 사용

--- lecture/Notepad/src/views/todo/container/TodoContainer.jsx
+++ lecture/Notepad/src/views/todo/container/TodoContainer.jsx
@@ -1,46 +1,23 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 import TodoTemplate from '../component/TodoTemplate';
 import TodoInsert from '../component/TodoInsert';
 import TodoList from '../component/TodoList';
-import todoData from '../../../@fake-db/todo';
+import { insert, toggle, remove } from '../../../modules/todos';
 
-function TodoContainer() {
-  const [todos, setTodos] = useState(todoData);
-
-  const nextId = useRef(todos.length + 1);
-
-  const handleAdd = useCallback((text) => {
-    const todo = {
-      id: nextId.current,
-      text,
-      checked: false,
-    };
-    setTodos((todos) => todos.concat(todo));
-    nextId.current++;
-  }, []);
-
-  const handleRemove = useCallback((id) => {
-    setTodos((todos) => todos.filter((todo) => todo.id !== id));
-  }, []);
-
-  const toggleTodoCheck = useCallback((id) => {
-    setTodos((todos) =>
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-      ),
-    );
-  }, []);
-
+function TodoContainer(props) {
+  const { todos, insert, toggle, remove } = props;
   return (
     <TodoTemplate>
-      <TodoInsert handleAdd={handleAdd} />
-      <TodoList
-        todos={todos}
-        handleRemove={handleRemove}
-        toggleTodoCheck={toggleTodoCheck}
-      />
+      <TodoInsert handleAdd={insert} />
+      <TodoList todos={todos} handleRemove={remove} toggleTodoCheck={toggle} />
     </TodoTemplate>
   );
 }
+const mapStateToProps = (state) => ({
+  todos: state.todos.todos,
+});
 
-export default TodoContainer;
+export default connect(mapStateToProps, { insert, toggle, remove })(
+  TodoContainer,
+);
