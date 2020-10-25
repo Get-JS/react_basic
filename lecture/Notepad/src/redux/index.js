62113cc7edb8264cf5efa299e48fb5e220a8cf4c
yjkwon07 <dudwns9073@nate.com>
2020-10-25 18:18:52 +0900

    (remove) lecture [Notepad > Profile]: - 의미 없는 페이지 삭제

--- lecture/Notepad/src/redux/index.js
+++ lecture/Notepad/src/redux/index.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux';
 import user from './user/reducer';
-import profile from './profile';
 import todo from './todo/reducer';
 import counter from './counter/reducer';
 import loading from './loading';
 
 const rootReducer = combineReducers({
   user,
-  profile,
   todo,
   counter,
   loading,
